da983897adcd499fc3baf7f5e63ca28e3f1a9192
Ashish Umrao <ashishumrao123456@gmail.com>
2025-04-18 02:40:29 +0530

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
 import Nav from './components/Nav';
-import Book from './components/Book'
-        
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
 
   return (
-    <>
+    <div className='App'>
+
+    <Router>
     <Nav  />  
-    <main style={{maxWidth:'1300px', margin:'auto', padding:'10px'}}>
-
-    <h3>You'll Like it</h3>
-    <div style={{display:'flex', overflowX:'scroll', gap:'15px', scrollbarWidth:'none'}}>
-    <Book img='https://img.wattpad.com/cover/313739483-256-k577904.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/251948264-256-k513244.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/362219749-256-k525210.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/105872-256-k921587.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/369058684-256-k52894.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/362219749-256-k525210.jpg' tags={["Fan Fiction", "Dark"]}/>
+      <Routes>
+        <Route path='/gittpad' element={<HomePage/>}/>
+      </Routes>
+    </Router>
     </div>
 
-    <br />
-    <br />
-
-    <h3>Trending Now &#x2197;</h3>
-    <div style={{display:'flex', overflowX:'scroll', gap:'15px', scrollbarWidth:'none'}}>
-    <Book img='https://img.wattpad.com/cover/313739483-256-k577904.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/251948264-256-k513244.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/362219749-256-k525210.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/105872-256-k921587.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/369058684-256-k52894.jpg' tags={["Fan Fiction", "Dark"]}/>
-    <Book img='https://img.wattpad.com/cover/362219749-256-k525210.jpg' tags={["Fan Fiction", "Dark"]}/>
-    </div>
-
-    <br /><br />
-
-    </main>
-    </> 
   )
 }
 
